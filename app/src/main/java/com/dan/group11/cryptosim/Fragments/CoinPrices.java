bd3c04c1027728402ddfb2d4969ce7ed4c5c9fbb
ec16467 <d.beg@se16.qmul.ac.uk>
2018-03-20 23:02:19 +0000

    Added ListView for Coins in Coin Price

--- app/src/main/java/com/dan/group11/cryptosim/Fragments/CoinPrices.java
+++ app/src/main/java/com/dan/group11/cryptosim/Fragments/CoinPrices.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.dan.group11.cryptosim.R;
 
@@ -14,10 +16,23 @@ import com.dan.group11.cryptosim.R;
  */
 
 public class CoinPrices extends Fragment {
+
+    ArrayAdapter adapter;
+    ListView listView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.coin_prices, container, false);
+
+        String[] array = {"Bitcoin", "Ethereum", "Ripple"};
+
+        adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, array);
+
+        listView = (ListView) myView.findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
         return myView;
     }
+
 }
--- app/src/main/java/com/dan/group11/cryptosim/MainActivity.java
+++ app/src/main/java/com/dan/group11/cryptosim/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.dan.group11.cryptosim.Fragments.CoinPrices;
 import com.dan.group11.cryptosim.Fragments.Login;

b859f3a74ade75ec944ac0b86c2960d550115ad0
ec16467 <d.beg@se16.qmul.ac.uk>
2018-03-21 16:34:03 +0000

    Added test listview and log in

---
+++ app/src/main/java/com/dan/group11/cryptosim/Activites/CoinPrices.java
@@ -0,0 +1,13 @@
+package com.dan.group11.cryptosim.Activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoinPrices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin_prices);
+    }
+}
---
+++ app/src/main/java/com/dan/group11/cryptosim/Adapter/CoinAdapter.java
@@ -0,0 +1,10 @@
+package com.dan.group11.cryptosim.Adapter;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by daniyaalbeg on 21/03/2018.
+ */
+
+public class CoinAdapter extends ArrayAdapter {
+}
